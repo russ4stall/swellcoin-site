11e4492910e1ec4c1883787083f603e7e4167d8f
Russ Forstall <forstallr@infinity-software.com>
2017-04-21 13:17:02 -0400

    mobile nav

--- js/site.js
+++ js/site.js
@@ -1,9 +1,21 @@
 $(document).ready(function () {
 
-    // Navbar
+    // Mobile Navbar toggle
     $("#nav-toggle").click(function () {
         $(this).toggleClass('open');
         $("#main-nav ul").slideToggle();
+
+        // delay change of logo color 
+        var $navTitle = $("#nav-title");
+        var delayTime = 80;
+        if ($navTitle.hasClass("open")) {
+            delayTime = 300;
+        }
+
+        $($navTitle).delay(delayTime).queue(function(next){
+            $(this).toggleClass('open');
+            next();
+        });
     });
 
 }); 
